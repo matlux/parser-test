9fa029143e1391881445a0f1c2e529a548f4e01c
Mathieu Gauthron <mathieu@matlux.net>
2015-02-15 23:30:58 +0000

    fixed the compilation and tests

--- src/main/scala/net/matlux/parsers/Reader.scala
+++ src/main/scala/net/matlux/parsers/Reader.scala
@@ -12,25 +12,19 @@ import java.util.UUID
  * The reader can be extended to handle more "tagged literals" in the pattern match in tagElem below
  */
 object Reader extends JavaTokenParsers {
-  val set: Parser[Set[Any]] = "#{" ~> rep(elem) <~ "}" ^^ (Set() ++ _)
-  val map: Parser[Map[Any, Any]] = "{" ~> rep(pair) <~ "}" ^^ (Map() ++ _)
-  val vector: Parser[Vector[Any]] = "[" ~> rep(elem) <~ "]" ^^ (Vector() ++ _)
+
   val list: Parser[List[Any]] = "(" ~> rep(elem) <~ ")"
   val keyword: Parser[Keyword] = ":" ~> """[^,#\"\{\}\[\]\(\)\s]+""".r ^^ (Keyword.intern(_))
   val symbol: Parser[clojure.lang.Symbol] = """[a-zA-Z][^,#\"\{\}\[\]\(\)\s]*""".r ^^ (clojure.lang.Symbol.create(_))
   lazy val pair: Parser[(Any, Any)] = elem ~ elem ^^ {
     case key ~ value => (key, value)
   }
-  lazy val tagElem: Parser[Any] = """#[^,#\"\{\}\[\]\(\)\s]+""".r ~ elem ^^ {
-    case "#uuid" ~ (value: String) => UUID.fromString(value)
-    case "#inst" ~ (value: String) => Instant.read(value)
-    case name ~ value => (name, value)
-  }
+
   val ratio: Parser[Double] = floatingPointNumber ~ "/" ~ floatingPointNumber ^^ {
     case num ~ _ ~ denom => num.toDouble / denom.toDouble
   }
 
-  val ednElem: Parser[Any] =  set | map | vector | list | keyword | tagElem | ratio |
+  val ednElem: Parser[Any] =  list | keyword | ratio |
                               wholeNumber <~ not('.') ^^ (_.toInt) |
                               floatingPointNumber     ^^ (_.toDouble) |
                               "nil"                   ^^ (_ => null)  |
--- src/test/scala/net/matlux/parsers/ReaderTest.scala
+++ src/test/scala/net/matlux/parsers/ReaderTest.scala
@@ -7,27 +7,12 @@ import clojure.lang.Symbol
 
 class ReaderTest extends FunSuite {
 
-  test("set") {
-    expectResult(Set()) { Reader.readAll("#{}") }
-    expectResult(Set(1)) { Reader.readAll("#{1}") }
-    expectResult(Set(1)) { Reader.readAll("#{1 1}") }
-    expectResult(Set(Set())) { Reader.readAll("#{#{}}") }
-  }
-
-  test("vector") {
-    expectResult(Vector()) { Reader.readAll("[]") }
-    expectResult(Vector(1)) { Reader.readAll("[1]") }
-    expectResult(Vector(1,1)) { Reader.readAll("[1 1]") }
-    expectResult(Vector(Vector())) { Reader.readAll("[[]]") }
-    expectResult(Vector(Vector(), Set())) { Reader.readAll("[[] #{}]") }
-  }
 
   test("list") {
     expectResult(List()) { Reader.readAll("()") }
     expectResult(List(1)) { Reader.readAll("(1)") }
     expectResult(List(1,1)) { Reader.readAll("(1 1)") }
     expectResult(List(List())) { Reader.readAll("(())") }
-    expectResult(List(List(), List(), Set())) { Reader.readAll("(() [] #{})") }
   }
 
   test("keyword") {
@@ -46,56 +31,5 @@ class ReaderTest extends FunSuite {
   }
 
 
-  test("map") {
-    expectResult(Map()) { Reader.readAll("{}") }
-    expectResult(Map(Keyword.intern("a") -> 1)) { Reader.readAll("{:a 1}") }
-    expectResult(Map(Keyword.intern("a") -> 1, Keyword.intern("b") -> 2)) { Reader.readAll("{:a 1 :b 2}") }
-    expectResult(Map(Map() -> Map())) { Reader.readAll("{{} {}}") }
-    expectResult(Map(Map() -> Map(), Set() -> Vector(), List() -> Keyword.intern("a")))
-    { Reader.readAll("{{} {} #{} [] () :a}") }
-  }
 
-  test("true / false / nil ") {
-    expectResult(true) { Reader.readAll("true") }
-    expectResult(false) { Reader.readAll("false") }
-    expectResult(null) { Reader.readAll("nil") }
-    expectResult(List(true, false, List(null))) { Reader.readAll("(true false (nil))") }
-  }
-
-  test("#uuid") {
-    val uuidStr = "f81d4fae-7dec-11d0-a765-00a0c91e6bf6"
-    expectResult(UUID.fromString(uuidStr)) { Reader.readAll("#uuid\"" + uuidStr + "\"") }
-    expectResult(List(UUID.fromString(uuidStr))) { Reader.readAll("(#uuid\"" + uuidStr + "\")") }
-  }
-
-  test("#inst") {
-    val dateStr = "2012-01-01T01:23:45.000-00:00"
-    expectResult(Instant.read(dateStr)) { Reader.readAll("#inst \"" + dateStr + "\"") }
-    expectResult(List(Instant.read(dateStr))) { Reader.readAll("(#inst \"" + dateStr + "\")") }
-  }
-
-  test("commas") {
-    expectResult(1) { Reader.readAll(",,,1") }
-    // expectResult(1) { Reader.readAll("1,,,") }   // TODO FIX
-    expectResult(Vector(1,2,3)) { Reader.readAll("[1,2,3]") }
-    expectResult(Map(Keyword.intern("a") -> 1, Keyword.intern("b") -> 2)) { Reader.readAll("{:a 1, :b 2}") }
-  }
-
-  test("strings") {
-    intercept[RuntimeException] { Reader.readAll("") }
-    expectResult("foo") { Reader.readAll("\"foo\"") }
-    expectResult(Map("foo" -> Vector("bar"))) { Reader.readAll{"{\"foo\" [\"bar\"]}"} }
-  }
-
-  // non edn spec stuff (still accepted by 'clojure.edn)
-
-  test("ratio") {
-    expectResult(0.5) { Reader.readAll("1/2") }
-    expectResult(Set(0.5, 1)) { Reader.readAll("#{1/2 42/42}") }
-  }
-
-  test("numbers with N") {
-    expectResult(List(42, 1)) { Reader.readAll("(42 1)") }
-    expectResult(List(42.5, 1)) { Reader.readAll("(42.5 1)") }
-  }
 }
